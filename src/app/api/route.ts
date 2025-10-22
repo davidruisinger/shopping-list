2d220456ac9b234a4e5d140cde0fc6e1dae43116
David Ruisinger <david.ruisinger@gmail.com>
2025-10-22 14:40:18 +0200

    Initial test API route

---
+++ src/app/api/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({
+    items: [
+      "Karotten",
+      "Knoblauch",
+      "Zwiebeln",
+      "Joghurt",
+      "Hafermilch",
+      "Käse",
+      "Tomaten",
+      "Paprika",
+      "Klopapier",
+    ],
+  });
+}
+
+export async function POST(request: Request) {
+  const res = await request.json();
+  return Response.json({ res });
+}
